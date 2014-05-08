2c13ce9965e14727a42f5b01badc81a7961eccc4
Jeremy Swinarton <jeremy@swinarton.com>
2014-05-08 17:44:06 -0400

    update version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages
 
 setup(
     name='pymarketo',
-    version='0.1',
+    version='0.1.1',
     description='Python interface to Marketo',
     author='Jeremy Swinarton',
     author_email='jeremy@swinarton.com',
